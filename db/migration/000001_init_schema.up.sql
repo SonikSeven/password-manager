8ec93b121abfbd5aa7ed9f488ddddbe01d04473c
SonikSeven <misha368@yahoo.com>
2025-09-24 18:35:32 +0200

    Initial commit

---
+++ db/migration/000001_init_schema.up.sql
@@ -0,0 +1,11 @@
+CREATE TABLE "users" (
+    "id" SERIAL PRIMARY KEY,
+    "username" VARCHAR(255) NOT NULL UNIQUE,
+    "email" VARCHAR(255) NOT NULL UNIQUE,
+    "password_hash" VARCHAR(255) NOT NULL,
+    "url" VARCHAR(255),
+    "notes" TEXT,
+    "icon" VARCHAR(255),
+    "created_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
